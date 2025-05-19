5e96adf0bad0759859b786e461dbb9a74cfbe7c6
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-19 10:11:13 +0530

    onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Product from "./Product";
+import { useState } from "react";
 
 function App() {
+
+ const [state,setStet] = useState(0);
+ const [text,setText] = useState("");
   return (
     <>
       <div>
@@ -9,7 +13,14 @@ function App() {
         <Product title="product 1" decs="product 1 decsription" price={34343} stock={4} />
         <Product title="product 1" decs="product 1 decsription" price={34343} stock={7} />
         <Product title="product 1" decs="product 1 decsription" price={34343} stock={0} />
-        
+        <button onClick={()=>{
+          setStet(state + 1)
+        }}>count button</button>
+        <p>{state}</p>
+
+
+        <input type="text" onChange={(e)=>{setText(e.target.value);}}/>
+        <p>{text}</p>
       </div>
     </>
   )
