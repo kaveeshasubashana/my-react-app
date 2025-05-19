1a1a1d36bdc03776a6217a727ea62550d9786aa1
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-19 09:01:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Product from "./Product";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+        <Product/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Product.jsx
@@ -0,0 +1,13 @@
+function Product(){
+
+    return (
+
+        <div>
+        <h1>my product</h1>
+        <p>this is my product component</p>
+     
+        </div>
+    );
+}
+
+export default Product;
